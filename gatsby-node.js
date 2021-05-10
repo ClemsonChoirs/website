6663150ddc88215a456edc95ccb1f20e2be9bd11
Nicholas Gustafson <nichgus7@gmail.com>
2021-05-10 16:33:02 -0400

    Updating npm modules

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,6 @@
 const _ = require('lodash')
 const path = require('path')
 const { createFilePath } = require('gatsby-source-filesystem')
-const { fmImagesToRelative } = require('gatsby-remark-relative-images')
 
 require("dotenv").config({
   path: `.env`,
@@ -52,7 +51,6 @@ exports.createPages = ({ actions, graphql }) => {
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
   const { createNodeField } = actions
-  fmImagesToRelative(node) // convert image paths for gatsby images
 
   if (node.internal.type === `MarkdownRemark`) {
     const value = createFilePath({ node, getNode })
